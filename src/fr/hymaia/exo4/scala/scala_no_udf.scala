bda9f862baf312a808cf20722a54b6eec06cd1d4
nafrawi <ahmedennaifer00@gmail.com>
2024-06-30 00:13:15 +0200

    removed useless files

--- src/fr/hymaia/exo4/scala/scala_no_udf.scala
+++ src/fr/hymaia/exo4/scala/scala_no_udf.scala
@@ -4,7 +4,6 @@ import org.apache.spark.sql.types._
 
 object ProjetSparkApp {
   def main(args: Array[String]): Unit = {
-    // Suppress Spark logging
     val spark = SparkSession.builder
       .appName("SellCategoryApp")
       .config("spark.master", "local[*]")
@@ -13,20 +12,14 @@ object ProjetSparkApp {
       .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
-
-    // Measuring time for reading CSV file
     val readStartTime = System.nanoTime()
     val sellData = readCsv(spark, "/home/nafra/Bureau/code/spark-handson/src/resources/exo4/sell.csv")
     val readEndTime = System.nanoTime()
     println(s"Time taken to read CSV: ${(readEndTime - readStartTime) / 1e6} ms")
-
-    // Adding category_name column
     val operationStartTime = System.nanoTime()
     val transformedData = addCategoryNameColumn(sellData)
     val operationEndTime = System.nanoTime()
     println(s"Time taken for transformation: ${(operationEndTime - operationStartTime) / 1e6} ms")
-
-    // Measuring time for writing Parquet file
     val writeStartTime = System.nanoTime()
     writeParquet(transformedData, "/home/nafra/Bureau/code/spark-handson/src/resources/exo4/sell_output.parquet")
     val writeEndTime = System.nanoTime()
@@ -54,8 +47,7 @@ object ProjetSparkApp {
   }
 
   def writeParquet(df: DataFrame, path: String): Unit = {
-    df.repartition(200)  // Adjust the number of partitions as needed
-      .write
+    df.write
       .mode("overwrite")
       .parquet(path)
   }
